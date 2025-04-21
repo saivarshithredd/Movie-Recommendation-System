489f58db3fecb48e5a9447260f9264b4ffed8494
saivarshithredd <svarshith57@gmail.com>
2025-04-21 21:02:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def home():
     return render_template('index.html', recommendations=recommendations, error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
